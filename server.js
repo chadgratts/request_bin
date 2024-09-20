0ff4233f12adc38d47ea93ec044449f14daa43ff
Jason Jones <JsunJ.Jones@gmail.com>
2024-09-20 09:25:54 -0400

    fix for secure websockets

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const app = require('./app');
 const WebSocket = require('ws');
 const { initWebSocketForBin } = require('./websockets/webSockets');
 const logger = require('./utils/logger');
+const fs = require('fs');
 
 // Express server
-const server = http.createServer(app);
+const server = http.createServer({
+  key: fs.readFileSync('...'),
+  cert: fs.readFileSync('...'),
+}, app);
 const port = 3000;
 server.listen(port, () => {
   logger.info(`Server is running on port ${port}`);
